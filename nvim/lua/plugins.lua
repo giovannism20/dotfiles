a160318c0c99f404f6bdb004939c15406a523fe1
Giovanni Martins <gmartins@disroot.org>
2021-03-15 18:16:01 -0300

    add config stuffs for status line

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -3,6 +3,8 @@ vim.cmd [[packadd packer.nvim]]
 vim.cmd([[autocmd BufWritePost plugins.lua PackerCompile ]])
 
 return require('packer').startup(function(use)
+  use {"wbthomason/packer.nvim", opt = true}
+
   -- colorscheme
   use {"npxbr/gruvbox.nvim", requires = {"rktjmp/lush.nvim"}}
 
